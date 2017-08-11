567e55523282d0243e5d44c6a02ac2c5c1312d01
LEE <ljh0326s@gmail.com>
2017-08-11 17:15:20 +0900

    finish Exercise3

--- src/ch4/Operator14.java
+++ src/ch4/Operator14.java
@@ -5,25 +5,6 @@ import java.util.Scanner;
 public class Operator14 {
 
 	public static void main(String[] args) {
-		int num;
-		int sum = 0;
-		boolean flag = true;
 		
-		
-		System.out.println("합게를 구할 숫자를 입력");
-		
-		while(flag) {
-			System.out.print(">>");
-			
-			Scanner scanner = new Scanner(System.in);
-			num = scanner.nextInt();
-			
-			if(num != 0) {
-				sum += num;
-			}
-			else
-				flag = false;
-		}
-		System.out.println(sum);
 	}
 }
